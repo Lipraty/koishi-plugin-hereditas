0fa5fa532bec51ef91fb775cb1f3b87492d67095
Lipraty <i@lonay.me>
2026-01-07 14:26:43 +0800

    chore: add lint ingnore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,22 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+  ignores: [
+    '**/lib/**',
+    '**/dist/**',
+    '**/*.d.ts',
+    '**/node_modules/**',
+  ],
   typescript: {
     overrides: {
       'ts/no-namespace': 'off',
       'ts/consistent-type-imports': 'off',
+      'ts/ban-ts-comment': 'off',
+      'no-console': 'off',
+      'unused-imports/no-unused-vars': 'warn',
     },
   },
+  vue: false,
+  jsonc: false,
+  yaml: false,
 })
